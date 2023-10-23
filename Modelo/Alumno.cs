77c2e8ec54a3a9db71cf0b93e1ff0263cb0c77e2
Minato01101 <SP23002ues.edu.sv>
2023-10-23 10:32:08 -0600

    Agregar archivos de proyecto.

---
+++ Modelo/Alumno.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Diccionario_Imprecion.Modelo
+{
+    class Alumno
+    {
+        public string numeroCarnet { get; set; }
+        public string nombre { get; set; }
+        public double[] notas { get; set; }
+
+        public double promedio { get; set; }
+
+        public void guerdarDatos()
+        {
+            //Se pide al usuario que ingrese sus datos
+            this.notas = new double[4]; 
+            Console.WriteLine("Ingrese el numero de carnet");
+            this.numeroCarnet = Console.ReadLine();
+            Console.WriteLine("Ingrese el nombre");
+            this.nombre = Console.ReadLine();
+
+            //Se solicita al usario que ingrese las notas
+            for (int i = 0; i < this.notas.Length; i++)
+            {
+                Console.Write($"Ingrese la nota {i+1}: ");
+                notas[i] = double.Parse(Console.ReadLine());
+            }
+            calcularPromedio();
+
+        }
+
+        public void calcularPromedio()
+        {
+            double suma = 0;
+            for (int i = 0; i < this.notas.Length; i++)
+            {
+                suma += notas[i];
+            }
+
+            this.promedio = suma/this.notas.Length;
+        }
+
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine($"Numero de carnet -> {this.numeroCarnet}");
+            Console.WriteLine($"Nombre -> {this.nombre}");
+            Console.WriteLine($"Promedio -> {this.promedio}");
+            Console.WriteLine(this.promedio < 6? "Reprobado":"Aprobado");
+            Console.WriteLine("NOTAS: ");
+            for (int i = 0; i < this.notas.Length; i++)
+            {
+                Console.Write($"\t {notas[i]}");
+            }
+
+        }
+    }
+}
+ 
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using Diccionario_Imprecion.Modelo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Diccionario_Imprecion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Materia;
+            string numeroGrupos;
+            Dictionary<String, Dictionary<String, List<Alumno>>> Matricula;
+            Matricula = new Dictionary<string, Dictionary<string, List<Alumno>>>();
+
+            do
+            {
+                Dictionary<String, List<Alumno>> dicionarioGrupos;
+                dicionarioGrupos = new Dictionary<string, List<Alumno>>();
+                Console.WriteLine("Ingrese el nombre de la materia");
+                Materia = Console.ReadLine();
+                //Alumno alumnoItem = new Alumno();
+                //List<Alumno> listaAlumno = new List<Alumno>();
+
+                do
+                {
+                    List<Alumno> listaAlumno = new List<Alumno>();
+                    Console.WriteLine($"Ingrese el numero de grupo para materia {Materia}");
+                    numeroGrupos = Console.ReadLine();
+
+                    do
+                    {
+                        Alumno alumnoItem = new Alumno();
+                        alumnoItem.guerdarDatos();
+                        listaAlumno.Add(alumnoItem);
+
+                    } while (validarCondicion("otro Alumno"));
+
+                    dicionarioGrupos.Add(numeroGrupos,listaAlumno); 
+                } while (validarCondicion("otro Grupo"));
+
+                Matricula.Add(Materia, dicionarioGrupos);
+            } while (validarCondicion("otra Materia"));
+
+
+            foreach (var itemMatricula in Matricula)
+            {
+                Console.WriteLine($"MATERIA -> {itemMatricula.Key}");
+
+                foreach (var itemGrupo in itemMatricula.Value)
+                {
+                    Console.WriteLine($"GRUPO -> {itemGrupo.Key}");
+                    foreach (var itemAlumnos in itemGrupo.Value)
+                    {
+                        itemAlumnos.MostrarDatos();
+                    }
+                }
+            }
+        }
+
+        public static bool validarCondicion(string dato)
+        {
+            string condicion = "";
+            Console.WriteLine($"Desea ingresar {dato} Preciones s para si\n cualquier tecla nono");
+            condicion = Console.ReadLine();
+
+            if(condicion.Equals("s", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
